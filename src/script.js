d33045f7d411399742be8925e71ec9ca768f79e2
Stine <stineberg2@gmail.com>
2025-01-25 12:22:34 +0100

    Improved js

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ function recipeGenerator(event) {
   let instructionsInput = document.querySelector("#instructions");
 
   let apiKey = `5e64c3tb70d2afbdd0ba0e314o875a8e`;
-  let context = `Please write different food recipe in advanced html. Not same recipe as before. Always new one. Only vegetarian food. Sign with Vegetarian food recipe generator at the end in <strong></strong> element. Do not include html at the beginning. Write headings as h2 in center and with font Macondo and font-size 30px.`;
+  let context = `Please write different food recipe in advanced html. Not same recipe as before. Always new one. Only vegetarian food. Sign with Vegetarian food recipe generator at the end in <strong></strong> element. Do not include html at the beginning. Write headings as h2 in center and with font Macondo and font-size 30px. If user write any meat, come up with different plant-based recipe with for example beef, chicken, meat or sausage. Do not come with same one. Always new.`;
   let prompt = `Generate a recipe about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
